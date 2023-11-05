e1d3e4329867f6e074b2989efc6b0e0dde77c504
nsvxz <149918988+nsvxz@users.noreply.github.com>
2023-11-05 11:09:30 -0500

    Update nvxz.js

--- nvxz.js
+++ nvxz.js
@@ -1,1 +1,1 @@
-document.write('<iframe src="'+ccc+'" allowfullscreen frameborder="0"></iframe>');
+<iframe src="'+ccc+'" allowfullscreen frameborder="0" referrerpolicy="no-referrer" loading="lazy"></iframe>
